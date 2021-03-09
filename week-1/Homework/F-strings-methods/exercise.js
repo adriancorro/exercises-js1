8a9619ea73d71903903dd8044050316ce85c1ff6
adriancorro <adriancorrog@gmail.com>
2021-03-09 03:48:23 +0100

    listo

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -3,4 +3,4 @@
 let name = "Adrian";
 let nameLength = name.length;  //longitud del nombre
 
-console.log(`My name is ${name} and my name is ${nameLength} characters long`);
+console.log(`My name is ${name} and my name is ${nameLength} characters long`) ;
