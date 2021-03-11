44ebd5a4f231ca19f85004451be8b636f3ce4d4b
adriancorro <adriancorrog@gmail.com>
2021-03-11 12:35:21 +0100

    week-3 extra 1 listo                faltan los demas del extra week3

--- week-3/Extra/1-oxygen-levels.js
+++ week-3/Extra/1-oxygen-levels.js
@@ -1,3 +1,4 @@
+
 /*
 Many years into the future, a team of Space Voyagers find their ship is low on Oxygen and need to dock
 somewhere safe while they call home for help.
@@ -9,8 +10,10 @@ To be safe to land on, a planet needs to have an Oxygen level between 19.5% and 
 Write a function that finds the first safe oxygen level in the array - Oxygen between 19.5% and 23.5%
 */
 
-function safeLevels() {
-
+function safeLevels(array) {
+    let firstValue =  array.find(  a => parseFloat(a)  > 19.5 &&   parseFloat(a) <  23.5   )
+    console.log(` first value found: ${firstValue}`)
+    return firstValue;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
